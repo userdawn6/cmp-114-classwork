e9c2aea90963e5ea63ac2f80b06ba11201a54a93
userdawn6 <dawn.chimereze@binghamuni.edu.ng>
2023-07-11 01:37:39 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,35 @@
+# Calculator README
+
+This code provides a simple calculator program that allows users to perform mathematical and physics calculations. The program is written in Python and provides a user-friendly interface for performing various operations.
+
+## Features
+
+The calculator program includes the following features:
+
+### Mathematics Operations
+
+- Addition: Perform addition of two numbers.
+- Subtraction: Perform subtraction of two numbers.
+- Multiplication: Perform multiplication of two numbers.
+- Division: Perform division of two numbers.
+- Exponentiation: Calculate the exponentiation of a base number raised to a given exponent.
+
+### Physics Operations
+
+- Velocity: Calculate the velocity by dividing the distance traveled by the time taken.
+- Acceleration: Calculate the acceleration by dividing the change in velocity by the time taken.
+- Force: Calculate the force using the formula mass times acceleration.
+- Momentum: Calculate the momentum using the formula mass times velocity.
+- Work: Calculate the work done using the formula force times distance.
+
+## Usage
+
+To use the calculator program, follow these steps:
+
+1. Run the Python program.
+2. Enter the subject category you would like to calculate: "Mathematics" or "Physics".
+3. Enter the operation you want to perform based on the selected category.
+4. Follow the prompts and enter the required values.
+5. The program will calculate the result and display it on the screen.
+
+Note: Make sure you have Python installed on your computer to run the program.
